2cd605c7b19a900f572ceba7ba2f685d9859b1d8
Pascal Chorus <p.chorus@me.com>
2020-07-01 20:22:33 +0200

    feat(contact): add contact page

---
+++ src/pages/NachrichtGesendet.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    <div class="u-max-page-width">
+      <h1 class="heading heading--xxl heading--xxl-paragraph u-margin-top-l">Vielen Dank!</h1>
+
+      <p class="u-font-m">Ihre Nachricht wurde gesendet.</p>
+      <p class="u-font-m u-font-m--paragraph">Ich werde mich schnellstmöglich bei Ihnen melden.</p>
+
+      <g-link to="/">Zurück zur Startseite</g-link>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Nachricht gesendet',
+  },
+};
+</script>
