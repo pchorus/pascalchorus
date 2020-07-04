7411ba0635d654c943072052a499ce2088329510
Pascal Chorus <p.chorus@me.com>
2020-07-04 21:33:20 +0200

    feat(services): add services page

---
+++ src/pages/Leistungen.vue
@@ -0,0 +1,416 @@
+<template>
+  <Layout>
+    <div>
+      <section class="section">
+        <h1 class="heading heading--xxl heading--xxl-paragraph u-max-page-width">
+          Leistungen
+        </h1>
+        <p class="u-font-m u-max-page-width">
+          Auf dieser Seite möchte ich Ihnen vorstellen, welche Leistungen ich bieten kann und was Sie von mir erwarten
+          dürfen.
+        </p>
+      </section>
+
+      <section class="section section--alternate-background">
+        <h2 class="heading heading--l heading--l-paragraph u-max-page-width">
+          Produkte
+        </h2>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Sie brauchen professionelle Unterstützung beim Entwickeln Ihrer maßgeschneiderten Software? Ich biete Ihnen
+          Konzeption, Entwicklung und Wartung folgender Produkte:
+        </p>
+
+        <h3 class="heading heading--m heading--m-paragraph u-max-page-width">
+          Webseiten
+        </h3>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Ich entwerfe Ihre individuelle Webseite nach aktuellen Maßstäben von Usability und User Experience mit
+          moderner Optik. Dabei lege ich Wert auf barrierefreie Webseiten, die allen Menschen zur Verfügung stehen. Sie
+          sind für Suchmaschinen optimiert (SEO) und mittels TLS verschlüsselt, sodass sie absolut sicher die Webseite
+          entdecken können. Alle Webseiten sind für verschiedene Gerätekategorien optimiert ("Responsive Design"),
+          sodass auf Smartphones, Tablets und Desktop-PCs stets das beste Benutzererlebnis erreicht wird.
+        </p>
+        <!--
+          - Performance - JAMstack
+          - Usability
+          - User Experience
+          - Accessibility
+          - SEO
+          - Sicherheit durch HTTPS
+          - responsive Webdesign: optimiert für Smartphone, Tablet und Desktop
+        -->
+        <h3 class="heading heading--m heading--m-paragraph u-max-page-width">
+          Webanwendungen
+        </h3>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Soll Ihr Internetauftritt mehr als nur statischen Inhalt bieten, kann es sinnvoll sein, eine sogenannte Single
+          Page Application (SPA) zu erstellen. SPAs sind Anwendungen, die im Browser laufen und umfangreiche
+          Funktionalität bieten wie zum Beispiel Online Shops, Bilderverwaltung, Chats und Ähnliches.
+        </p>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Dabei verfolge ich den gleichen Qualitätsansatz wie bei Webseiten: Usability, User Experience,
+          Barrierefreiheit, TLS-Verschlüsselung und Responsive Design werden auch hier stets berücksichtigt.
+        </p>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Um die volle Funktionalität Ihrer Webanwendung zu ermöglichen, können externe Datendienste (APIs) angebunden
+          werden. Darüber hinaus biete ich die Implementierung eigener Server-Logik, bei Bedarf auch mit Anbindung von
+          Datenbanken oder Content Management Systemen (CMS).
+        </p>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Sollten Sie Ihre Webanwendung mit einer passenden Homepage oder Landingpage bewerben wollen, ist es sinnvoll,
+          beide Konzepte zu vereinen: Eine statische Webseite dient als Landingpage, um die Aufmerksamkeit Ihrer
+          Interessenten und Kunden zu gewinnen. Von dort aus kann Ihre eigentliche Webanwendung dann gestartet werden.
+        </p>
+        <!--
+          - SPA -- Single Page Application
+          - Serverless Functions oder richtiges Backend, bei Bedarf mit Datenbank
+        -->
+
+        <h3 class="heading heading--m heading--m-paragraph u-max-page-width">
+          Desktopanwendungen
+        </h3>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Neben Webanwendungen gibt es weiterhin die klassiche Desktopanwendung. Mit modernen Technologien ist es heute
+          möglich, klassische Desktopanwendungen mit Hilfe von Webtechnologien umzusetzen. Dabei profitieren Sie von
+          modern gestalteten Benutzeroberflächen, die mehr können als native Anwendungen für Ihr Betriebssystem wie
+          Windows, Linux oder macOS.
+        </p>
+        <p class="u-font-m u-max-page-width">
+          Darüber hinaus ist es möglich, mit geringem Zusatzaufwand Ihre Anwendung als Webanwendung und Desktopanwendung
+          zu erstellen, da beide Anwendungen auf der gleichen Codebasis aufbauen.
+        </p>
+        <!--
+          - gleiche Technologien wie für Web können verwendet werden
+          - gleicher Code kann für Web und Native App genutzt werden ⇒ Kombination möglich, Desktop-App und Web-App auf
+            gleicher Codebasis
+        -->
+      </section>
+
+      <section class="section">
+        <h2 class="heading heading--l heading--l-paragraph u-max-page-width">
+          Software-Entwicklung von A bis Z
+        </h2>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Ob sie eine neue Webseite brauchen oder eine individuelle Software zur Optimierung Ihrer Geschäftsprozesse,
+          ich begleite Sie gerne über den gesamten Entwicklungsprozess hinweg.
+        </p>
+
+        <h3 class="heading heading--m heading--m-paragraph u-max-page-width">
+          Analyse und Sammlung von Anforderungen
+        </h3>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          In einem ersten Gespräch erarbeiten wir gemeinsam, welche Fragen Ihre Webseite beantworten, welche
+          Problemstellung Ihre Software lösen oder bei welchen Aufgabenstellungen Ihre Software unterstützen soll.
+        </p>
+
+        <h3 class="heading heading--m heading--m-paragraph u-max-page-width">
+          Beratung und Erarbeitung von Vorschlägen
+        </h3>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Basierend auf den Analyseergebnissen erarbeite ich individuelle Vorschläge und berate Sie detailliert, wie die
+          gesteckten Ziele erreicht werden können.
+        </p>
+
+        <h3 class="heading heading--m heading--m-paragraph u-max-page-width">
+          Umsetzung
+        </h3>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Ist eine gemeinsame Vorstellung der Software erarbeitet und das Zielbild gezeichnet, beginne ich mit der
+          Umsetzung. Dabei lege ich in regelmäßigen Abständen Zwischenergebnisse vor, sodass Sie stets den Fortschritt
+          im Auge behalten und zeitnah Änderungswünsche adressieren können.
+        </p>
+
+        <h3 class="heading heading--m heading--m-paragraph u-max-page-width">
+          Wartung und Datensicherung
+        </h3>
+        <p class="u-font-m u-max-page-width">
+          Nach Fertigstellung und Inbetriebnahme Ihrer Software unterstütze ich gerne, den einwandfreien Betrieb
+          sicherzustellen. Dazu gehört eine regelmäßige Aktualisierung aller Software-Komponenten, das Monitoring des
+          Betriebs sowie die Sicherung gespeicherter Daten.
+        </p>
+      </section>
+
+      <section class="section section--alternate-background">
+        <h2 class="heading heading--l heading--l-paragraph u-max-page-width">
+          Meine Arbeitsweise
+        </h2>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Bei der Entwicklung von Software gibt es ein paar Grundprinzipien, denen ich folge:
+        </p>
+        <p class="u-font-m u-max-page-width">
+          Gerne arbeite ich agil nach dem Scrum-Manifest. Dadurch kann ich kurzfristig und individuell auf sich ändernde
+          Anforderungen eingehen. Der Vorteil liegt auf der Hand: In kurzen Zeitabschnitten werden Zwischenergebnisse
+          bewertet und die nächsten Zwischenziele entsprechend justiert. Somit werden Entwicklungsressourcen stets
+          optimal und zielgerichtet eingesetzt.
+        </p>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Alternativ ist auch eine Realisierung mit festem Zeitplan und klar definierter Zielvorstellung möglich.
+        </p>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Durch konsequentes Testen mit Hilfe automatisierter Unit-Tests und E2E-Tests stelle ich die einwandfreie
+          Funktionalität Ihrer Webseite oder Anwendung sicher. Dabei wird die Software bei Änderungen automatisch
+          geprüft und die Bedienung durch einen Benutzer simuliert.
+        </p>
+        <p class="u-font-m u-max-page-width">
+          Statische Codeanalyse, Linting und automatisierte Code-Formatierung mit Tools wie Prettier verhindern schon
+          während der Entwicklungszeit Fehler. Somit ist eine schnelle und fehlerfreie Entwicklung bereits während des
+          Programmierens sichergestellt.
+        </p>
+      </section>
+
+      <section class="section">
+        <h2 class="heading heading--l heading--l-paragraph u-max-page-width">
+          Technologien
+        </h2>
+        <p class="u-font-m u-font-m--paragraph u-max-page-width">
+          Zur Umsetzung der Projekte setze ich unter anderem folgende Technologien ein. Sollten Sie Unterstützung mit
+          anderen Technologien benötigen, sprechen Sie mich gerne an.
+        </p>
+        <ul class="u-max-page-width">
+          <li class="u-margin-bottom-xl">
+            <h3 class="heading heading--m heading--m-paragraph">Frontend</h3>
+            <ul class="tech-list">
+              <li class="tech-list__item" v-for="item in frontendTechnologies">
+                <image-with-title :image="item.image" :title="item.title" :key="item.title"></image-with-title>
+              </li>
+            </ul>
+          </li>
+          <li class="u-margin-bottom-xl">
+            <h3 class="heading heading--m heading--m-paragraph">Backend</h3>
+            <ul class="tech-list">
+              <li class="tech-list__item" v-for="item in backendTechnologies">
+                <image-with-title :image="item.image" :title="item.title" :key="item.title"></image-with-title>
+              </li>
+            </ul>
+          </li>
+          <li class="u-margin-bottom-xl">
+            <h3 class="heading heading--m heading--m-paragraph">Headless CMS</h3>
+            <ul class="tech-list">
+              <li class="tech-list__item" v-for="item in headlessCmsTechnologies">
+                <image-with-title :image="item.image" :title="item.title" :key="item.title"></image-with-title>
+              </li>
+            </ul>
+          </li>
+          <li class="u-margin-bottom-xl">
+            <h3 class="heading heading--m heading--m-paragraph">Hosting</h3>
+            <ul class="tech-list">
+              <li class="tech-list__item" v-for="item in hostingTechnologies">
+                <image-with-title :image="item.image" :title="item.title" :key="item.title"></image-with-title>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3 class="heading heading--m heading--m-paragraph">Quellcode & Qualität</h3>
+            <ul class="tech-list">
+              <li class="tech-list__item" v-for="item in sourceCodeAndQualityTechnologies">
+                <image-with-title :image="item.image" :title="item.title" :key="item.title"></image-with-title>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import ImageWithTitle from '../components/ImageWithTitle';
+
+export default {
+  components: { ImageWithTitle },
+  metaInfo: {
+    title: 'Leistungen',
+  },
+  data: function () {
+    return {
+      frontendTechnologies: [
+        {
+          image: {
+            path: '../technologies/html5.svg',
+            altText: 'HTML5-Logo',
+          },
+          title: 'HTML5 und CSS3',
+        },
+        {
+          image: {
+            path: '../technologies/sass.svg',
+            altText: 'Sass-Logo',
+          },
+          title: 'Sass',
+        },
+        {
+          image: {
+            path: '../technologies/javascript.svg',
+            altText: 'JavaScript-Logo',
+          },
+          title: 'JavaScript',
+        },
+        {
+          image: {
+            path: '../technologies/typescript.svg',
+            altText: 'TypeScript-Logo',
+          },
+          title: 'TypeScript',
+        },
+        {
+          image: {
+            path: '../technologies/angular.svg',
+            altText: 'Angular-Logo',
+          },
+          title: 'Angular',
+        },
+        {
+          image: {
+            path: '../technologies/vue.png',
+            altText: 'Vue.js-Logo',
+          },
+          title: 'Vue.js',
+        },
+        {
+          image: {
+            path: '../technologies/gridsome.svg',
+            altText: 'Gridsome-Logo',
+          },
+          title: 'Gridsome',
+        },
+      ],
+      backendTechnologies: [
+        {
+          image: {
+            path: '../technologies/nodejs.svg',
+            altText: 'Nodejs-Logo',
+          },
+          title: 'Node.js',
+        },
+        {
+          image: {
+            path: '../technologies/postgres.png',
+            altText: 'PostreSQL-Logo',
+          },
+          title: 'PostgreSQL',
+        },
+      ],
+      headlessCmsTechnologies: [
+        {
+          image: {
+            path: '../technologies/netlify.svg',
+            altText: 'netlifycms-Logo',
+          },
+          title: 'netlifycms',
+        },
+        {
+          image: {
+            path: '../technologies/sanity.svg',
+            altText: 'Sanity-Logo',
+          },
+          title: 'Sanity',
+        },
+        {
+          image: {
+            path: '../technologies/prismic.svg',
+            altText: 'Prismic-Logo',
+          },
+          title: 'Prismic',
+        },
+      ],
+      hostingTechnologies: [
+        {
+          image: {
+            path: '../technologies/netlify.svg',
+            altText: 'Netlify-Logo',
+          },
+          title: 'Netlify',
+        },
+        {
+          image: {
+            path: '../technologies/firebase.svg',
+            altText: 'Firebase-Logo',
+          },
+          title: 'Firebase',
+        },
+        {
+          image: {
+            path: '../technologies/heroku.svg',
+            altText: 'Heroku-Logo',
+          },
+          title: 'Heroku',
+        },
+        {
+          image: {
+            path: '../technologies/aws.png',
+            altText: 'AWS-Logo',
+          },
+          title: 'Amazon Web Services',
+        },
+      ],
+      sourceCodeAndQualityTechnologies: [
+        {
+          image: {
+            path: '../technologies/git.svg',
+            altText: 'Git-Logo',
+          },
+          title: 'Git',
+        },
+        {
+          image: {
+            path: '../technologies/github.png',
+            altText: 'GitHub-Logo',
+          },
+          title: 'GitHub',
+        },
+        {
+          image: {
+            path: '../technologies/sentry.svg',
+            altText: 'Sentry-Logo',
+          },
+          title: 'Sentry',
+        },
+        {
+          image: {
+            path: '../technologies/google_analytics.svg',
+            altText: 'Google Analytics-Logo',
+          },
+          title: 'Google Analytics',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/variables';
+
+.section {
+  padding-top: $space-xl;
+  padding-bottom: $space-xl;
+
+  &--alternate-background {
+    background-color: $background-color-gray;
+  }
+}
+
+.tech-list {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: $space-l;
+}
+
+@media (max-width: $max-width-mobile-landscape) {
+  .tech-list {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+// IE 11 fallback
+@media (-ms-high-contrast: active), (-ms-high-contrast: none) {
+  .tech-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    &__item {
+      margin-right: $space-m;
+    }
+  }
+}
+</style>
